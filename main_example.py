45c16333e24be611fb1e5140d32bd8d77d0cae18
ravindaa <ravindaa@oregonstate.edu>
2022-04-02 22:19:45 +0600

    Fixed menu

--- main_example.py
+++ main_example.py
@@ -8,7 +8,7 @@ button_menu_def = ['File', ['&Open', ['1','2','3'], 'Save', 'Properties', 'Exit'
 
 layout = [
     [sg.Menu(menu_def,text_color='black', font="SYSTEM_DEFAULT", pad=(10,10))],
-    [sg.ButtonMenu('Button Menu', [['nf', 'op','om','---','rf','cl'],['New File', 'Open','Open Module','---','Recent Files','Close']], key='-B_MENU-', tooltip="Button Menu", visible=True)],
+    [sg.ButtonMenu('Button Menu', menu_def=button_menu_def, image_filename="menu.png", border_width=5, key='-B_MENU-')],
     [sg.Multiline(size=(80,10),tooltip='Write your Text here')],
     [sg.Text('File Name'),sg.Input(),sg.OptionMenu(values=['.txt','.pdf','.gif', '.jpg','.mp4','.gif','.dat','.sql'],size=(4,8), default_value='.doc',key='-DATA_TYPE-')],
     [sg.Button('SAVE'),sg.Button('CANCEL')]
@@ -25,8 +25,8 @@ while True:
     elif event=='SAVE':
         print(values['-DATA_TYPE-'])
 
-    if values['-B_MENU-'] == "Recent Files":
-        print("Selected Recent Files")
+    if values['-B_MENU-'] == "Properties":
+        print("Selected Properties")
 
 
 window.close()
